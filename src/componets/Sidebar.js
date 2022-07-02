46adb8f8f12e2c9361ecdda37f1c0121c01a3776
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-07-02 05:10:01 +0300

    fixed note selection

--- src/componets/Sidebar.js
+++ src/componets/Sidebar.js
@@ -9,6 +9,14 @@ const SideBar = ({
 
   const sortedNotes = notes.sort((a,b) => b.date - a.date)
 
+  const selectNote = (e) => {
+    const note = e.target.closest('.note')
+
+    if (!e.target.classList.contains('remove-btn')) {
+      setActiveNote(note.id)
+    }
+  }
+
   return (
 
     <div className="sidebar">
@@ -20,18 +28,17 @@ const SideBar = ({
       <div className="note-list">
         {sortedNotes.map((note) => {
           return (
-            <div className={`note ${note.id === activeNote ? 'active' : ''}`} 
-            id={ note.id } key={ note.id }
-            onClick={() => setActiveNote(note.id)}
+            <div
+             key={note.id}
+             className={`note ${note.id === activeNote && 'active'}`} 
+             id={note.id}
+             onClick={selectNote}
             >
               <div className="note-data">
                 <h4 className="note-title">{ note.title }</h4>
                 <small className="note-date">
-                  Last Modified{' '}
-                  {new Date(note.date).toLocaleString({
-                    hour: '2-digit',
-                    minute: '2 digit'
-                  })}
+                  Updated:{' '}
+                  {new Date(note.date).toLocaleString()}
                 </small>
               </div>
               <button className="remove-btn" onClick={() => deleteNote(note.id)}>Remove</button>
